1d60c08e6d76e67583d9c08230e300378b07f778
unknown <ajameslusk@gmail.com>
2018-04-20 12:36:09 -0400

    added test for recipes update

--- test/test-server-recipes.js
+++ test/test-server-recipes.js
@@ -55,4 +55,29 @@ describe('Recipes', function(){
       });
   });
 
+  it('should update items on PUT', function() {
+    
+    const updateData = {
+      name: 'foo Toast',
+      ingredients: ['foo', 'bread']
+    };
+
+    return chai.request(app)
+      
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updateData);
+      });
+  });
 });
